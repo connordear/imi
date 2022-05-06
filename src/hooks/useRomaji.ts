72c7ee4f99caba013e11bc936b0842fdd22df892
Connor Dear <connordear@gmail.com>
2022-05-05 20:46:39 -0600

    added additional place phrases and expanded dictionary

--- src/hooks/useRomaji.ts
+++ src/hooks/useRomaji.ts
@@ -3,6 +3,7 @@ import { toRomaji as toRomajiWanaKana } from "wanakana";
 // Use this to override some of default romaji translations
 const romajiDict = new Map<string, string>();
 romajiDict.set("は", "wa");
+romajiDict.set("には", "niwa");
 romajiDict.set("出身", "shusshin");
 romajiDict.set("日本", "nihon");
 
--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -8,7 +8,17 @@ translate.key = process.env.REACT_APP_DEEPL;
 const jpToEnDict = new Map();
 jpToEnDict.set("出身", "place where one is born");
 jpToEnDict.set("は", "is");
-
+jpToEnDict.set("あります", "has");
+jpToEnDict.set("ありますか", "you have?");
+jpToEnDict.set("に", "(position article)");
+jpToEnDict.set("にほん", "Japan");
+jpToEnDict.set("すんでいますか", "you live?");
+jpToEnDict.set("いきたいですか", "do you want to go?");
+jpToEnDict.set("いったことが", "been to");
+jpToEnDict.set("ごねん", "5 years");
+jpToEnDict.set("いきましたか", "did you go?");
+jpToEnDict.set("いきました", "went");
+jpToEnDict.set("いきますか", "are you going?");
 export function useTranslationQuery(japanese = "") {
   return useQuery(["jp", japanese], async () => {
     if (jpToEnDict.has(japanese)) {
